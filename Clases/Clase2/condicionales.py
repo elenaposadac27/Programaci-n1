df102efb40805b3270db87a1f0da4efafb2019c4
elenaposadac27 <posada.elena@uces.edu.co>
2020-02-19 13:29:29 -0500

    condicionales e inputs

---
+++ Clases/Clase2/condicionales.py
@@ -0,0 +1,15 @@
+#Condicionales
+#Para preguntar si es igual se utiliza ==
+#Para preguntar si es mayor o menor se utiliza > y <
+#Para preguntar si es difrente se utiliza !=
+
+
+A = 2
+B = 3
+print(A == B, B > A, B < A, A != B)
+
+HOLA = "Hola mundo que hace"
+MUNDO = "mundo"
+CUANDO = "cuando"
+print(MUNDO in HOLA)
+print(CUANDO in HOLA)
---
+++ Clases/Clase2/inputs_ejemplo.py
@@ -0,0 +1,22 @@
+
+#---------------Mensajes----------------
+#int es cuando la respuesta es numero ENTEROS
+#float es cuando la respuesta es numeros DECIMALES
+PREGUNTA_NOMBRE = "ingrese su nombre \n "
+MENSAJE_BIENVENIDO_A = "Bienvenido"
+MENSAJE_BIENVENIDO_B = "a este programa"
+PREGUNTA_EDAD = "ingrese su edad \n "
+MENSAJE_EDAD = "Tu edad es"
+PREGUNTA_ESTATURA = "ingrese su estatura \n "
+MENSAJE_ESTATURA = "Tu estatura es"
+#---------------------------------------
+
+
+_nombrePersona = input(PREGUNTA_NOMBRE)
+print(MENSAJE_BIENVENIDO_A, _nombrePersona, MENSAJE_BIENVENIDO_B)
+_edadPersona = int(input(PREGUNTA_EDAD))
+print(MENSAJE_EDAD, _edadPersona)
+print (type(_edadPersona))
+_estaturaPersona = float(input(PREGUNTA_ESTATURA))
+print(MENSAJE_ESTATURA, _estaturaPersona)
+print (type(_estaturaPersona))
