0fe4636f6762615d4c10b49dc73c52293f378d64
elenaposadac27 <posada.elena@uces.edu.co>
2020-02-19 12:12:02 -0500

    Hola mundo

---
+++ Clases/Clase1/Empresa.py
@@ -0,0 +1,10 @@
+#Ejercicio empresa
+nombre = "La paleteria"
+print (nombre)
+print(type(nombre))
+antiguedad = 3
+print (antiguedad)
+print (type(antiguedad))
+capital = 5000.99
+print (capital)
+print (type(capital))
---
+++ Clases/Clase1/holamundo.py
@@ -0,0 +1,11 @@
+#Codigo primera clase
+nombre = "Elena"
+print (nombre)
+print (type(nombre))
+edad = 19
+print (edad)
+print (type(edad))
+dinero = 999.99
+print (dinero)
+print (type(dinero))
+
