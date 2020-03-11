07b09475249681744df5c746c1ccc743b26ededd
elenaposadac27 <posada.elena@uces.edu.co>
2020-03-11 13:13:31 -0500

    subiendo calculadora

---
+++ Clases/clase7/calculadora.py
@@ -0,0 +1,17 @@
+def suma(x,y) :
+    suma = x+y
+    return suma
+
+def resta(x,y) :
+    resta = x-y
+    return resta
+
+def multiplicacion(x,y) :
+    multiplica = x*y
+    return multiplica
+
+def division(x,y) :
+    divide = x/y
+    return divide
+
+print(multiplicacion(10,2))
