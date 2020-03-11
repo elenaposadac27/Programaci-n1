49aacc6b93ca517114d3359290959c9259d7df95
elenaposadac27 <posada.elena@uces.edu.co>
2020-03-11 13:01:29 -0500

    subiendo funciones

---
+++ Clases/clase6/funciones.py
@@ -0,0 +1,11 @@
+#----Anotaciones---
+#def es para definir alfuna funcion en especifico (solo definir)
+
+def marco():
+    print("#"*30)
+def saludar(_nombre):
+    print("Bienvenido(a)", _nombre, "a este codigo")
+
+marco()
+saludar("Elena")
+saludar(input("Ingrese su nombre \n"))
