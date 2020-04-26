65d26acf1efb152c3243bf04c581b36d2b0effec
elenaposadac27 <posada.elena@uces.edu.co>
2020-04-25 20:04:25 -0500

    subiendo clase pandas

--- Clases/clase12/pandas_ejemplo.py
+++ Clases/clase12/pandas_ejemplo.py
@@ -8,25 +8,26 @@ import pandas as pd
 
 #Asi: 
 # diccionario =p.read_csv("estudiantes.csv",encoding='UTF-8', header = 0,delimiter=';').to_dict()
-print(diccionario)
+#   print(diccionario)
+
 #para imprimir como los titulitos del cuador de excel:
-print(diccionario.keys())
+#   print(diccionario.keys())
 #para imprimir los datos dentro de determinada columna (los datos dento de la columna de determinado titulito):
-print(diccionario["Nombre"])
+#   print(diccionario["Nombre"])
 
 #valor maximo dentro de los datos de determinada columna segun tamaño (longitud de palabras)
-mayor_nombre = max (diccionario["Nombre"].values(), key=len)
+#   mayor_nombre = max (diccionario["Nombre"].values(), key=len)
 #valor maximo dentro de los datos de determinada columna segun tamaño
-minimo_nombre = min (diccionario["Nombre"].values(), key=len)
+#   minimo_nombre = min (diccionario["Nombre"].values(), key=len)
 
-print("\n\nel estudiante con el nombre mas largo es {} y el que presenta el nombre mas corto es {} ".format(mayor_nombre, minimo_nombre))
+#   print("\n\nel estudiante con el nombre mas largo es {} y el que presenta el nombre mas corto es {} ".format(mayor_nombre, minimo_nombre))
 
 
-estudiante_mayor = max(diccionario["Edad"].values())
-estudiante_menor = min(diccionario["Edad"].values())
-print(estudiante_mayor)
-print(estudiante_menor)
-nota_mayor = max( diccionario["PARCIAL 2"].values())
-nota_menor = min( diccionario["PARCIAL 2"].values())
-print(nota_mayor)
-print(nota_menor)
+#   estudiante_mayor = max(diccionario["Edad"].values())
+#   estudiante_menor = min(diccionario["Edad"].values())
+#   print(estudiante_mayor)
+#   print(estudiante_menor)
+#   nota_mayor = max( diccionario["PARCIAL 2"].values())
+#   nota_menor = min( diccionario["PARCIAL 2"].values())
+#   print(nota_mayor)
+#   print(nota_menor)
